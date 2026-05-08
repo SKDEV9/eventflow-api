b558c9793e3a9adf37f4162f9b265e316191440c
Samuel <skgomes06@gmail.com>
2026-05-08 10:21:05 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/SKDEV9/eventflow_api/EventflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.SKDEV9.eventflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventflowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SKDEV9/eventflow_api/EventflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SKDEV9.eventflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
